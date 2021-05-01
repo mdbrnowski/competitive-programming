c16023088195cd7abe34658ba4bf3f2fde3275ed
Michał Dobranowski <mdbrnowski@gmail.com>
2021-05-01 22:12:07 +0200

    Create 1519B.cpp

---
+++ Codeforces/1519B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        cout << (n * m - 1 == k ? "YES" : "NO") << '\n';
+    }
+    return 0;
+}
