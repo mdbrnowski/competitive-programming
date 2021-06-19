ea7c4d74d0d57e7cb552da779087e183646bd485
Michał Dobranowski <mdbrnowski@gmail.com>
2021-06-19 18:40:05 +0200

    Create 1537B.cpp

---
+++ Codeforces/1537B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << n << ' ' << 1 << ' ' << 1 << ' ' << m << '\n';
+    }
+    return 0;
+}
