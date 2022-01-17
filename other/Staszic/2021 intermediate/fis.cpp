35ce6e28d2fc73ba8fa4f5cd2f04f9c0cc6e6bfb
Michał Dobranowski <mdbrnowski@gmail.com>
2022-01-17 19:29:33 +0100

    Create fis.cpp

---
+++ other/Staszic/2021 intermediate/fis.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+constexpr int MOD = 1e9+7;
+
+pair<ll, ll> fib (ll n) {
+    if (n == 0)
+        return {0, 1};
+    auto [p, r] = fib(n / 2);
+    ll c = (p * (2 * r - p + MOD)) % MOD;
+    ll d = (p * p + r * r) % MOD;
+    if (n & 1)
+        return {d, (c + d) % MOD};
+    else
+        return {c, d};
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll a, b;
+    cin >> a >> b;
+    a++, b++;
+    cout << (fib(b + 2).first - fib(a + 1).first + MOD) % MOD << '\n';
+    return 0;
+}
