b1649530649fe225bfa72b306c8609d9b47fe996
Michał Dobranowski <mdbrnowski@gmail.com>
2024-10-13 12:33:57 +0200

    Create 1036A.cpp

---
+++ Codeforces/1036A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll n, k;
+    cin >> n >> k;
+    cout << (k + n - 1) / n << '\n';
+    return 0;
+}
