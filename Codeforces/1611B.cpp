933c65d74b002246ad654f674e1503ccc2acc21e
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-04 23:38:32 +0200

    Create 1611B.cpp

---
+++ Codeforces/1611B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        cout << min({(n + m) / 4, n, m}) << '\n';
+    }
+}
