e28f7ca5cb0665bd4ded0c40d87c258e38e38356
Michał Dobranowski <mdbrnowski@gmail.com>
2025-09-09 21:27:24 +0200

    Create 2140B.cpp

---
+++ Codeforces/2140B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+
+        cout << 2 * x << '\n';
+    }
+}
